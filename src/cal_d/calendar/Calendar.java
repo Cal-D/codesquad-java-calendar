c2b1cac61a09d8fc32a914fefa2ae563e9b30536
DESKTOP-I050P82\Administrator <blackpinetree92@gmail.com>
2019-03-06 20:54:15 +0900

    Init calendar project

--- src/cal_d/calendar/Calendar.java
+++ src/cal_d/calendar/Calendar.java
@@ -3,8 +3,9 @@ package cal_d.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello calendar");
+		
+		
 	}
 
 }
